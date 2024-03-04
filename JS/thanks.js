5491f95544fa82c3857c19782c7be6754cc12cdc
Danny-245 <117903552+Danny-245@users.noreply.github.com>
2024-03-04 19:10:09 +0100

    Update thanks.js

--- JS/thanks.js
+++ JS/thanks.js
@@ -1,16 +1,18 @@
-// function redirect() {
-//     var count = 15;
-//     var countDownContent = document.getElementById("cd-num");
+function redirect() {
+    var count = 15;
+    var countDownContent = document.getElementById("cd-num");
 
-//     var countDownInterval = setInterval(() => {
-//         if (count > 0) {
-//             countDownContent.innerText = count;
-//             count--;
-//         }
-//         else {
-//             window.location.href = "index.html";
-//         }
-//     }, 1000);
-// }
+    var countDownInterval = setInterval(() => {
+        if (count > 0) {
+            countDownContent.innerText = count;
+            count--;
+        }
+        else {
+            window.location.href = "index.html";
+        }
+    }, 1000);
+}
 
-// redirect();
+redirect();
+
+//THANKS FOR SUBSCRIBING
