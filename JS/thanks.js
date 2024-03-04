e7b1a4aa057756dda7e521504d42f1f8a3bd3118
Danny-245 <117903552+Danny-245@users.noreply.github.com>
2024-03-04 18:15:49 +0100

    Add files via upload

---
+++ JS/thanks.js
@@ -0,0 +1,16 @@
+// function redirect() {
+//     var count = 15;
+//     var countDownContent = document.getElementById("cd-num");
+
+//     var countDownInterval = setInterval(() => {
+//         if (count > 0) {
+//             countDownContent.innerText = count;
+//             count--;
+//         }
+//         else {
+//             window.location.href = "index.html";
+//         }
+//     }, 1000);
+// }
+
+// redirect();
