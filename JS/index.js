e528fc4e944fcd6a1cefe7092728ad9ac04d1466
Danny-245 <117903552+Danny-245@users.noreply.github.com>
2024-03-11 18:55:28 +0100

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,12 @@
 const navBar = document.getElementById('top-nav');
 const navLogo = document.getElementById('logo');
 const navLink = document.getElementsByClassName('nav-link');
-window.onscroll = function() {
+window.onscroll = function () {
     scrollFunction();
     scrollFunction2();
 }
 function scrollFunction() {
-    if(document.body.scrollTop > 130 || document.documentElement.scrollTop > 130 ) {
+    if (document.body.scrollTop > 130 || document.documentElement.scrollTop > 130) {
         navBar.classList.add('apply');
         // navLink.classList.add('apply2');
         // navLogo.classList.add('apply');
@@ -19,21 +19,21 @@ function scrollFunction() {
     }
 }
 
-$(document).ready(function() {
-$(".counter").counterUp({
-    delay: 10,
-    time: 1200
-})
+$(document).ready(function () {
+    $(".counter").counterUp({
+        delay: 10,
+        time: 1200
+    })
 });
 
 const backToTopBtn = document.querySelector('.scrolltop');
 
 
 function scrollFunction2() {
-    if(document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         backToTopBtn.style.display = 'block';
     }
-    else{
+    else {
         backToTopBtn.style.display = 'none';
     }
 }
@@ -43,10 +43,43 @@ function topFunction() {
     document.documentElement.scrollTop = 0;
 }
 
-// backToTopBtn.addEventListener('click', scrollTop);
-
 const preLoad = document.getElementById('preload');
 function preloadFunction() {
     preLoad.style.display = 'none';
 }
 setTimeout(preloadFunction, 5000);
+console.log(preloadFunction.arguments);
+
+const subNews = document.getElementById('sub-news');
+
+function subscribe() {
+    subNews.style.display = 'none';
+
+}
+ 
+// save the email to local storage
+
+    document.addEventListener("DOMContentLoaded", function() {
+        setTimeout(()=> {
+            if(!localStorage.getItem("email")) {
+                subNews.style.display = "flex";
+            }
+            else {
+                subNews.style.display = "none";
+
+            }
+        }, 10000);
+    })
+
+    const emailForm = document.getElementById('pop-up');
+    emailForm.addEventListener("submit", (e)=> {
+        e.preventDefault();
+        const emailAuth = document.getElementById('email');
+        localStorage.setItem("email", emailAuth);
+        subNews.style.display = 'none';
+       
+    })
+    // localStorage.clear();
+
+    
+
